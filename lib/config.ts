fb5bcf3118ab28b63be42853cb3aaaecc9fcb444
Андрей Едунов <a.edunov@vk.team>
2024-02-23 20:42:10 +0300

    initiated

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+// TODO: change this to the notion ID of the page you want to test
+export const rootNotionPageId = "145978fad5a643fa93fd299196d8db7b";
+
+export const isDev =
+  process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
