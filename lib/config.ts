d6e12b048fd159da694ccc6763a65b6ffc88e20b
Андрей Едунов <a.edunov@vk.team>
2024-03-10 22:32:13 +0300

    new link and styles

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 // TODO: change this to the notion ID of the page you want to test
-export const rootNotionPageId = "145978fad5a643fa93fd299196d8db7b";
+export const rootNotionPageId = "20e314d3415c46a7bd43b2d641b87c6b";
 
 export const isDev =
   process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
